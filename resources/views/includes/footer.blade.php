ca9d1c3636f1e9227cac23a3784df14ea517579f
benzics <benjaminnicholas29@gmail.com>
2022-07-29 15:22:03 +0100

    Separated header and footer section to be included

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,145 @@
+<a href="#" id="back-to-top" class="back-to-top fa fa-arrow-up show-back-to-top"></a>
+<script>
+/*--window Scroll functions--*/
+$(window).on('scroll', function () {
+	/*--show and hide scroll to top --*/
+	var ScrollTop = $('#back-to-top');
+	if ($(window).scrollTop() > 500) {
+		ScrollTop.fadeIn(1000);
+	} else {
+		ScrollTop.fadeOut(1000);
+	}
+});
+</script>
+<div class="footer_bg center">
+	<div class="area">
+		<div class="bodycontainer">
+			<div style="z-index:2; position: relative">
+				<h2 class="margintb">GET STARTED TODAY WITH BITCOIN INVESTMENT</h2>
+				<p class="margintb">Open account for free and start trading Bitcoins!</p>
+				<a href="get-started" class="btn">GET STARTED</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<footer>
+    
+
+    
+    <!-- ConveyThis button: -->
+<div id="conveythis-wrapper-main"><a href="https://www.conveythis.com" class="conveythis-no-translate notranslate" title="ConveyThis" >ConveyThis</a></div>
+<script src="//s2.conveythis.com/javascriptClassic/1/conveythis.js"></script>
+<script src="//s2.conveythis.com/javascriptClassic/1/translate.js"></script>
+<script type="text/javascript">
+document.addEventListener("DOMContentLoaded", function(e) {
+conveythis.init({source_language_id: 703, languages: [{"id":"703","active":true},{"id":"704","active":false},{"id":"708","active":false},{"id":"709","active":false},{"id":"710","active":false},{"id":"711","active":false},{"id":"707","active":false},{"id":"712","active":false},{"id":"713","active":false},{"id":"714","active":false},{"id":"715","active":false},{"id":"716","active":false},{"id":"706","active":false},{"id":"717","active":false},{"id":"718","active":false},{"id":"806","active":false},{"id":"705","active":false},{"id":"719","active":false},{"id":"796","active":false},{"id":"798","active":false},{"id":"720","active":false},{"id":"723","active":false},{"id":"703","active":false},{"id":"724","active":false},{"id":"725","active":false},{"id":"722","active":false},{"id":"726","active":false},{"id":"727","active":false},{"id":"799","active":false},{"id":"728","active":false},{"id":"729","active":false},{"id":"730","active":false},{"id":"731","active":false},{"id":"732","active":false},{"id":"800","active":false},{"id":"733","active":false},{"id":"801","active":false},{"id":"734","active":false},{"id":"735","active":false},{"id":"736","active":false},{"id":"721","active":false},{"id":"802","active":false},{"id":"737","active":false},{"id":"738","active":false},{"id":"803","active":false},{"id":"775","active":false},{"id":"776","active":false},{"id":"797","active":false},{"id":"777","active":false},{"id":"778","active":false},{"id":"774","active":false},{"id":"779","active":false},{"id":"780","active":false},{"id":"781","active":false},{"id":"782","active":false},{"id":"783","active":false},{"id":"784","active":false},{"id":"785","active":false},{"id":"786","active":false},{"id":"787","active":false},{"id":"812","active":false},{"id":"811","active":false},{"id":"788","active":false},{"id":"789","active":false},{"id":"790","active":false},{"id":"813","active":false},{"id":"795","active":false},{"id":"794","active":false},{"id":"793","active":false},{"id":"792","active":false},{"id":"791","active":false},{"id":"810","active":false},{"id":"809","active":false},{"id":"773","active":false},{"id":"772","active":false},{"id":"808","active":false},{"id":"770","active":false},{"id":"769","active":false},{"id":"768","active":false},{"id":"767","active":false},{"id":"765","active":false},{"id":"807","active":false},{"id":"764","active":false},{"id":"763","active":false},{"id":"762","active":false},{"id":"766","active":false},{"id":"761","active":false},{"id":"760","active":false},{"id":"759","active":false},{"id":"758","active":false},{"id":"757","active":false},{"id":"755","active":false},{"id":"754","active":false},{"id":"753","active":false},{"id":"752","active":false},{"id":"751","active":false},{"id":"750","active":false},{"id":"749","active":false},{"id":"748","active":false},{"id":"805","active":false},{"id":"804","active":false},{"id":"746","active":false},{"id":"745","active":false},{"id":"744","active":false},{"id":"771","active":false},{"id":"756","active":false},{"id":"747","active":false},{"id":"743","active":false},{"id":"742","active":false},{"id":"814","active":false},{"id":"815","active":false},{"id":"739","active":false},{"id":"741","active":false},{"id":"740","active":false}]})
+});
+</script>
+<!-- End ConveyThis button code. -->
+
+    
+  
+  
+
+  
+  
+  
+  
+  
+    
+    
+    <div class="bodycontainer">
+        <div class="row">
+            <div class="col-2 col-m-4 col-sm-12">
+            <div class="padding">
+                <h3>QUICK LINKS</h3>
+                <ul>
+                    <li><a href="assets/">Home</a></li>
+                    <li><a href="assets/info/about">About Us</a></li>
+                    <li><a href="assets/faqs">FAQS</a></li>
+                    <li><a href="assets/info/terms">TERMS AND CONDITIONS</a></li>
+                    <li><a href="assets/contact">CONTACT US</a></li>
+                </ul>
+            </div>
+            </div>
+            <div class="col-2 col-m-4 col-sm-12">
+            <div class="padding">
+                <h3>HELP & SUPPORT</h3>
+                <ul>
+                    <li><a href="https://cointelegraph.com/bitcoin-for-beginners/what-are-cryptocurrencies" target="_blank">WHAT IS BITCOIN?</a></li>
+                    <li><a href="https://www.investopedia.com/tech/how-to-buy-bitcoin/" target="_blank">HOW TO BUY BITCOIN</a></li>
+                    <li><a href="assets/register">REGISTER</a></li>
+                    <li><a href="assets/login">LOGIN</a></li>
+					<li><a href="assets/fpass">FORGOT PASSWORD</a></li>
+                </ul>
+            </div>
+            </div>
+            <div class="col-3 col-m-4 col-sm-12">
+            <div class="padding">
+                <h3>CONTACT US</h3>
+                <ul>
+                    <li>Kloveniersburgwal 105II, 1011 KB Amsterdam, Netherlands</li>
+                </ul>
+				<a href="mailto://info@globaloptionsfxtrade.com"><h3>info@globaloptionsfxtrade.com</h3></a>
+				<a href="tel:+1 959 500 9190">+1 959 500 9190</a>
+				<ul>
+					<li>MON-SAT 08AM ⇾ 05PM</li>
+					<ul class="social">
+						<li><a href="" class="circle"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="" class="circle"><i class="fa fa-twitter"></i></a></li>
+					</ul>
+				</ul>
+            </div>
+            </div>
+            <div class="col-5 col-m-12 col-sm-12">
+				<div class="col-6 col-m-6 col-sm-6" style="padding-bottom:0">
+					<div class="value">$198.76B</div>
+					MARKET CAP
+					<div class="value">69K+</div>
+					ACTIVE ACCOUNTS
+				</div>
+				<div class="col-6 col-m-6 col-sm-6" style="padding-bottom:0">
+					<div class="value">243K</div>
+					WEEKLY TRANSACTIONS
+					<div class="value">127</div>
+					SUPPORTED COUNTRIES
+				</div>
+				<div class="col-12" style="padding-top:0">
+					<hr>
+					SUPPORTED PAYMENT METHODS<br>
+					<img src="assets/images/5b55bb652af1a.png" style="width:40px">
+					<img src="assets/images/1532345051h7.png" style="width:40px">
+					<img src="assets/images/1532345115h7.png" style="width:40px">
+				</div>
+            </div>
+        </div>
+	<hr>
+	<p class="center">Copyright © {{date('Y')}} Global Options FX Trade. All Rights Reserved</p>
+    </div>
+</footer>
+<script src="assets/js/particle.js"></script>
+<script>
+particlesJS("particles-js", {
+	"particles":{ "number":{"value":100,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}
+},"retina_detect":true});
+</script>
+<script>
+  (function(b,i,t,C,O,I,N) {
+    window.addEventListener('load',function() {
+      if(b.getElementById(C))return;
+      I=b.createElement(i),N=b.getElementsByTagName(i)[0];
+      I.src=t;I.id=C;N.parentNode.insertBefore(I, N);
+    },false)
+  })(document,'script','https://widgets.bitcoin.com/widget.js','btcwdgt');
+</script>
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    
+<link href="{{ asset('favicon.png') }}" rel="icon" type="image/x-icon" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+{{-- Our css --}}
+<link href="{{ asset('assets/css/main.css') }}" rel="stylesheet"/>
+<link href="{{ asset('assets/css/animate.min.css') }}" rel="stylesheet" />
+
+<link rel="manifest" href="{{ asset('assets/js/manifest.json') }}" />
+<meta name="theme-color" content="#FD961A">
+<meta name="msapplication-navbutton-color" content="#FD961A" />
+<!-- iOS Safari -->
+<meta name="apple-mobile-web-app-status-bar-style" content="#FD961A" />
+
+
+<meta name="theme-color" content="#FD961A" />
+<meta name="msapplication-navbutton-color" content="#FD961A" />
+<!-- iOS Safari -->
+<meta name="apple-mobile-web-app-status-bar-style" content="#FD961A" />
+
+<link rel="icon" sizes="192x192" href="{{ asset('/assets/images/icon/icon-192.png') }}">
+
+<meta name="keywords" content="Global Options FX Trade, cryptocurrency, investments" />
+<meta property="og:image" content="{{ asset('/assets/images/icon/icon-310x310.png') }}" />
+
+<meta name="msapplication-square310x310logo" content="{{ asset('/assets/images/icon/icon-310x310.png') }}" />
+<meta name="msapplication-square70x70logo" content="{{ asset('/assets/images/icon/icon-70x70.png') }}" />
+<meta name="msapplication-square150x150logo" content="{{ asset('/assets/images/icon/icon-150x150.png') }}" />
+<meta name="msapplication-wide310x150logo" content="{{ asset('/assets/images/icon/icon-310x150.png') }}" />
+
+
+<script src="{{ asset('/assets/js/jquery.min.js') }}"></script>
+<script src="{{ asset('/assets/js/jquery-ui.js') }}"></script>
+<script src="{{ asset('/assets/js/jquery.form.js') }}"></script>
+
+<link href="{{ asset('/assets/css/select2.min.css') }}" rel="stylesheet" />
+
+<script>
+    $(document).ready(function() {
+        function close_accordion_section() {
+            $('.accordion .accordion-section-title').removeClass('active');
+            $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+        }
+     
+        $('.accordion-section-title').click(function(e) {
+            // Grab current anchor value
+            var currentAttrValue = $(this).attr('href');
+     
+            if($(e.target).is('.active')) {
+                close_accordion_section();
+            }else {
+                close_accordion_section();
+     
+                // Add active class to section title
+                $(this).addClass('active');
+                // Open up the hidden content panel
+                $('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+            }
+     
+            e.preventDefault();
+        });
+    });
+    </script>
+    <script>
+    var fade_outthis = function() {
+        $(".alert").fadeOut().empty();
+    }
+    setTimeout(fade_outthis, 8000);
+    
+    $(document).ready(function(){
+        $(".closebtn").click(function(){
+            $(this).parent().fadeOut().empty();
+        });
+    });
+    </script>
+
+</head>
+<body class="loading_it">
+
+    <header class="header">
+
+     <!-- TradingView Widget BEGIN -->
+     <div class="tradingview-widget-container">
+        <div class="tradingview-widget-container__widget"></div>
+        <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+            {
+                "symbols": [
+                {
+                    "description": "Amazon",
+                    "proName": "NASDAQ:AMZN"
+                },
+                {
+                    "description": "Facebook",
+                    "proName": "NASDAQ:FB"
+                },
+                {
+                    "description": "Alphabet",
+                    "proName": "NASDAQ:GOOGL"
+                },
+                {
+                    "description": "Apple",
+                    "proName": "NASDAQ:AAPL"
+                },
+                {
+                    "description": "Microsoft",
+                    "proName": "NASDAQ:MSFT"
+                },
+                {
+                    "description": "BTC/USD",
+                    "proName": "COINBASE:BTCUSD"
+                },
+                {
+                    "description": " \tAlibaba Group Holding",
+                    "proName": " \tNYSE:BABA"
+                },
+                {
+                    "description": " \tBerkshire Hathaway",
+                    "proName": " \tNYSE:BRK.B"
+                },
+                {
+                    "description": " \tJPMorgan Chase",
+                    "proName": " \tNYSE:JPM"
+                },
+                {
+                    "description": "ETH/USD",
+                    "proName": "KRAKEN:ETHUSD"
+                },
+                {
+                    "description": " \tJohnson & Johnson",
+                    "proName": " \tNYSE:JNJ"
+                },
+                {
+                    "description": " \tExxon Mobil",
+                    "proName": " \tNYSE:XOM"
+                },
+                {
+                    "description": " \tRoyal Dutch Shell",
+                    "proName": " \tNYSE:RDS.A"
+                },
+                {
+                    "description": " \tBank of America",
+                    "proName": " \tNYSE:BAC"
+                },
+                {
+                    "description": "XRP/USD",
+                    "proName": "BITFINEX:XRPUSD"
+                }
+            ],
+                    "colorTheme": "dark",
+                    "isTransparent": false,
+                    "displayMode": "compact",
+                    "locale": "en"
+            }
+        </script>
+    </div>
+    <!-- TradingView Widget END -->
+    
+    
+    
+    
+    
+    
+<div class="container row">
+<ul class="topheader">
+	<li class="logo">
+		<div class="bodycontainer">
+			<button class="btn round default right menu"><i class="fa fa-align-right"></i></button>
+			<a href="{{ url('/') }}">
+				<img src="{{ asset('/assets/images/pro.jpg') }}" title="Global Options FX Trade"/>
+			</a>
+		</div>
+	</li>
+	<li class="data">
+		<div class="bodycontainer">
+			<ul>
+				<li><div>3,649,450 USD</div>Payouts</li>
+				<li><div>+5.26%</div>24 hour price</li>
+				<li><div>12.820 BTC</div>24 hour volume</li>
+				<li><div>69,775</div>active traders</li>
+				<li><div class="btcwdgt-price" bw-cur="usd"></div>Live Bitcoin price</li>
+			</ul>
+		</div>
+	</li>
+	<li class="account row">
+		<div class="bodycontainer">
+			<div class="button_container"><a href="{{ url('/login') }}" class="btn"><i class="fa fa-user"></i> SIGN IN</a></div>
+				<div class="button_container"><a href="{{ url('/register') }}" class="btn v2"><i class="fa fa-user-plus"></i> REGISTER</a></div>		</div>
+	</li>
+</ul>
+</div>
+</header>
+<nav class="center">
+	<div class="bodycontainer">
+		<ul>
+			<li><a href="{{ url('/') }}" class="active">HOME</a></li>
+			<li><a href="{{ url('/info/about') }}">ABOUT US</a></li>
+			<li><a href="{{ url('#investment') }}">INVESTMENT PLANS</a></li>
+			<li><a href="#">LEGAL <i class="fa fa-chevron-down"></i></a>
+				<ul>
+					<li><a href="{{ url('/info/terms') }}">TERMS & CONDITIONS</a></li>
+					<li><a href="{{ url('/faqs') }}">FAQS</a></li>
+				</ul>
+			</li>
+			<li><a href="{{ url('/contact') }}">CONTACT US</a></li>
+			<li><a href="{{ url('/affiliate_program') }}">AFFILIATE</a></li>
+		</ul>
+	</div>
+</nav>
+<script>
+	$(".menu").click( function(){
+		$("nav").slideToggle();
+	});
+</script>
+
+<div class="preloader">
+<div id="bitcoin">
+  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="200px" height="200px" viewBox="100 100 400 400" xml:space="preserve">
+    <filter id="dropshadow" height="130%">
+  <feGaussianBlur in="SourceAlpha" stdDeviation="5"/>
+  <feOffset dx="0" dy="0" result="offsetblur"/>
+  <feFlood flood-color="red"/>
+<feComposite in2="offsetblur" operator="in"/>
+<feMerge>
+<feMergeNode/>
+<feMergeNode in="SourceGraphic"/>
+</feMerge>
+</filter>          
+<path class="path" style="filter:url(#dropshadow)" fill="#000000" d="M446.089,261.45c6.135-41.001-25.084-63.033-67.769-77.735l13.844-55.532l-33.801-8.424l-13.48,54.068
+	c-8.896-2.217-18.015-4.304-27.091-6.371l13.568-54.429l-33.776-8.424l-13.861,55.521c-7.354-1.676-14.575-3.328-21.587-5.073
+	l0.034-0.171l-46.617-11.64l-8.993,36.102c0,0,25.08,5.746,24.549,6.105c13.689,3.42,16.159,12.478,15.75,19.658L208.93,357.23
+	c-1.675,4.158-5.925,10.401-15.494,8.031c0.338,0.485-24.579-6.134-24.579-6.134l-9.631,40.468l36.843,9.188
+	c8.178,2.051,16.209,4.19,24.098,6.217l-13.978,56.17l33.764,8.424l13.852-55.571c9.235,2.499,18.186,4.813,26.948,6.995
+	l-13.802,55.309l33.801,8.424l13.994-56.061c57.648,10.902,100.998,6.502,119.237-45.627c14.705-41.979-0.731-66.193-31.06-81.984
+	C425.008,305.984,441.655,291.455,446.089,261.45z M368.859,369.754c-10.455,41.983-81.128,19.285-104.052,13.589l18.562-74.404
+	C306.28,314.65,379.774,325.975,368.859,369.754z M379.302,260.846c-9.527,38.187-68.358,18.781-87.442,14.023l16.828-67.489
+	C327.767,212.14,389.234,221.02,379.302,260.846z"/>
+          
+</svg>
+</div>
+</div>
+<script>
+/*--window load functions--*/
+jQuery(window).load(function(){
+	var preLoder = $(".preloader");
+	preLoder.fadeOut(1000);
+	$("body").removeClass("loading_it");
+});
+</script>
