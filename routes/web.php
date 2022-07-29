66fb06c213f3a9833fe798b8469523edb3a7f3be
benzics <benjaminnicholas29@gmail.com>
2022-07-29 15:28:04 +0100

    Starting registration page design

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegistrationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,4 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/register', [RegistrationController::class, 'index']);
