9ed7a46b8ae0897aa47e4c690a6ea2769b6944a0
benzics <benjaminnicholas29@gmail.com>
2022-07-29 19:22:21 +0100

    Added profiles table schema

---
+++ database/migrations/2022_07_29_181227_create_profiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('referrer');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('zip');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
