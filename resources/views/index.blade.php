21cc61eafb183aa68697c3fe5e409a51987a677b
benzics <benjaminnicholas29@gmail.com>
2022-07-29 09:42:34 +0100

    Added index controller and view for test

---
+++ resources/views/index.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    
+<link href="{{ asset('favicon.png') }}" rel="icon" type="image/x-icon" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+{{-- Our css --}}
+<link href="{{ asset('assets/css/main.css') }}" rel="stylesheet"/>
+<link href="{{ asset('assets/css/animate.min.css') }}" rel="stylesheet" />
+
+<link rel="manifest" href="{{ asset('assets/js/manifest.json') }}" />
+<meta name="theme-color" content="#FD961A">
+<meta name="msapplication-navbutton-color" content="#FD961A" />
+<!-- iOS Safari -->
+<meta name="apple-mobile-web-app-status-bar-style" content="#FD961A" />
+
+
+<meta name="theme-color" content="#FD961A" />
+<meta name="msapplication-navbutton-color" content="#FD961A" />
+<!-- iOS Safari -->
+<meta name="apple-mobile-web-app-status-bar-style" content="#FD961A" />
+
+<link rel="icon" sizes="192x192" href="{{ asset('/assets/images/icon/icon-192.png') }}">
+
+<meta name="keywords" content="Global Options FX Trade, cryptocurrency, investments" />
+<meta property="og:image" content="{{ asset('/assets/images/icon/icon-310x310.png') }}" />
+
+<meta name="msapplication-square310x310logo" content="{{ asset('/assets/images/icon/icon-310x310.png') }}" />
+<meta name="msapplication-square70x70logo" content="{{ asset('/assets/images/icon/icon-70x70.png') }}" />
+<meta name="msapplication-square150x150logo" content="{{ asset('/assets/images/icon/icon-150x150.png') }}" />
+<meta name="msapplication-wide310x150logo" content="{{ asset('/assets/images/icon/icon-310x150.png') }}" />
+</head>
+<body>
+    
+</body>
+</html>
