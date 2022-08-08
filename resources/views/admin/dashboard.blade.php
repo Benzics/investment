ba460ce85053121fbdf844b852abba2ede485e73
benzics <benjaminnicholas29@gmail.com>
2022-08-08 14:14:03 +0100

    Added font awesome cdn library

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@isset($title) {{ $title }} @endisset - Admin</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <link rel="stylesheet" href="{{ asset('/assets/css/admin.css') }}" />
 </head>
 <body>
@@ -15,10 +17,10 @@
             <nav class="bg-dark">
                 <ul class="side-nav">
                     <li class="active">
-                        <a href="#" class="active">Dashboard</a>
+                        <a href="#" class="active"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href="#">Users</a>
+                        <a href="#"><i class="fas fa-users"></i> Users</a>
                     </li>
                     <li>
                         <a href="#">Deposits</a>
