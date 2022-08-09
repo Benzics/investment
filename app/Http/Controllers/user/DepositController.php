9eae70c952a3fecd31cffcf516f728c689d2544b
benzics <benjaminnicholas29@gmail.com>
2022-08-09 17:03:16 +0100

    Wrote validation for deposit attachment upload

--- app/Http/Controllers/user/DepositController.php
+++ app/Http/Controllers/user/DepositController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\File;
 
 class DepositController extends Controller
 {
     public function index()
     {
         return view('user.deposit');
+    }
+
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            'attachment' => ['required', File::image()],
+            'amount' => 'required|min:1',
+        ]);
     }
 }
