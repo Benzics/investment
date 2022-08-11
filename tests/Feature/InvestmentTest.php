fb1c9957c9f5c3efa79dd6c13f11e3afbe4bf963
benzics <benjaminnicholas29@gmail.com>
2022-08-11 14:42:22 +0100

    Wrote initial tests for investment feature

---
+++ tests/Feature/InvestmentTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class InvestmentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void {
+        parent::setUp();
+        $this->artisan('db:seed');
+    }
+
+    public function test_user_investment_page()
+    {
+        $user = User::find(1);
+
+        $response = $this->actingAs($user)->get('/user/new-investment');
+
+        $response->assertOk();
+    }
+
+    public function test_user_investment_preview()
+    {
+        $user = User::find(1);
+
+        $data = [
+            'investment_id' => '1',
+        ];
+        $response = $this->actingAs($user)->post('/user/new-investment', $data);
+        $response->assertValid()->assertOk();
+    }
+
+    public function test_user_investment_create()
+    {
+        $user = User::find(1);
+
+        $data = [
+            'amount' => '100',
+            'investment_id' => '1',
+        ];
+
+        $response = $this->actingAs($user)->post('/user/invest', $data);
+        $response->assertValid()->assertSessionHas('success')->assertRedirect('/user/investments');
+    }
+
+    public function test_user_investments_list()
+    {
+        $user = User::find(1);
+
+        $response = $this->actingAs($user)->get('/user/investments');
+
+        $response->assertOk();
+    }
+}
