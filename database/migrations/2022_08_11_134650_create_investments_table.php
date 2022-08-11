3a369eec862f06555e5a80bcd7af09e28da21ef6
benzics <benjaminnicholas29@gmail.com>
2022-08-11 14:57:32 +0100

    Created investments table

---
+++ database/migrations/2022_08_11_134650_create_investments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('investments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('commission');
+            $table->float('minimum');
+            $table->float('maximum');
+            $table->enum('type', ['Daily', 'Weekly', 'Monthly', 'Yearly']);
+            $table->integer('times');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('investments');
+    }
+};
