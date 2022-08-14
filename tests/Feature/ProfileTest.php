9ab5875800abfb50842a8be42994351767827781
benzics <benjaminnicholas29@gmail.com>
2022-08-14 17:16:56 +0100

    Initial tests for user profile

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -2,21 +2,42 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProfileTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public $user;
+    public function setUp(): void {
+        parent::setUp();
+        $this->artisan('db:seed');
+        $this->user = User::find(1);
+    }
+
+    public function test_profile_page()
+    {
+        $response = $this->actingAs($this->user)->get('/user/profile');
+
+        $response->assertOk();
+    }
+
+    public function test_profile_edit()
+    {
+        $data =[
+            'name' => 'test',
+            'address' => 'test',
+            'gender' => 'male',
+            'country' => 'test',
+            'zip' => 'test',
+            'phone' => '',
+        ];
+
+        $response = $this->actingAs($this->user)->post('/user/edit-profile', $data);
+
+        $response->assertValid()->assertSessionHas('success');
     }
 }
