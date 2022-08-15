475f315b7f75dcd26029f981615c6e5f39436479
benzics <benjaminnicholas29@gmail.com>
2022-08-15 08:44:04 +0100

    Added status column on users db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('is_admin')->default('0');
+            $table->integer('status')->default('1');
             $table->rememberToken();
             $table->timestamps();
         });
