cee8cdfd85ca1a30973d1f33b300d7362df02671
benzics <benjaminnicholas29@gmail.com>
2022-08-19 20:12:32 +0100

    Investment table alter

---
+++ database/migrations/2022_08_19_190817_alter_investment_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('investments', function (Blueprint $table) {
+            $table->string('minimum')->change();
+            $table->string('maximum')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('investments', function (Blueprint $table) {
+            //
+        });
+    }
+};
