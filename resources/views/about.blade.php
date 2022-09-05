576583f932579b51e3346652e8c617acdf570bae
benzics <benjaminnicholas29@gmail.com>
2022-09-05 23:22:14 +0100

    about page edit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -31,14 +31,14 @@
      <h3 style="text-align: justify;">Bitcoin mining operation is carried out 24hr/day generating constant steady interest on the invested bitcoin. An accumulated interest rate is distributed evenly accross every wallet registered and connected to the mining network.</h3>
      <h3 style="text-align: justify;">At {{ $site_name }} we have the best group of mining expert and we mine using the best mining hardware( CGminer) which makes it possible for our clients to get the best mining services.</h3>
      <h3 style="text-align: justify;">Every wallet connected to the mining network gets a steady interest return which makes this investment platform more profiting and beneficial.</h3>
-     <h3 style="text-align: justify;">Once your account is created and registered, your wallet will be credited with a start up bonus interest&nbsp; of 0.0004btc from {{ $site_name }}</h3>
+     <h3 style="text-align: justify;">Once your account is created and registered, your wallet will be credited with a start up bonus interest&nbsp; of 0.0001btc from {{ $site_name }}</h3>
      <h3 style="text-align: justify;">You can monitor and control your investment on {{ $site_name }} 24hrs a day.</h3>
      <h3 style="text-align: justify;">At {{ $site_name }} mining network we will never ask our clients for money, we always advise our clients to disregard any such person requesting money from them.&nbsp;</h3>
      <h3 style="text-align: justify;">&nbsp;</h3>
      <h3 style="text-align: justify;">&nbsp;</h3>
      <h3 style="text-align: justify;">&nbsp;</h3>
      <h3 style="text-align: justify;">WITHDRAWAL METHOD</h3>
-     <h3 style="text-align: justify;">You can make a withdrawal after 7 days from your investment period.</h3>
+     <h3 style="text-align: justify;">You can make a withdrawal after 5 days from your investment period.</h3>
      <h3 style="text-align: justify;">&nbsp;</h3>
      <h3 style="text-align: justify;">COMMISSION AND COMPENSATION</h3>
      <h3 style="text-align: justify;">At {{ $site_name }} we will never ask you to send money to us neither would any of our online agents request money from you.&nbsp;</h3>
@@ -46,6 +46,6 @@
      <h3 style="text-align: justify;">&nbsp;</h3>
      <h3 style="text-align: justify;">NOTE: The Bitcoin miner compensation/commission is 25% of your weekly accumulated interest.</h3>
      <h3 style="text-align: justify;">Everytime you make a withdrawal, you are required to pay 25% mining compensation of your total weekly accumulated interest to the Bitcoin miner.&nbsp;</h3>
-     <h3 style="text-align: justify;">The 25% give back, is a compensation fee for the work that the Bitcoin miners do to help you multiply your income. Is a way of saying thank you for the good work done every week.</h3> </div><a href="#" id="back-to-top" class="back-to-top fa fa-arrow-up show-back-to-top"></a>
+     <h3 style="text-align: justify;">The 10% give back, is a compensation fee for the work that the Bitcoin miners do to help you multiply your income. Is a way of saying thank you for the good work done every week.</h3> </div><a href="#" id="back-to-top" class="back-to-top fa fa-arrow-up show-back-to-top"></a>
 
      @include('includes.footer')
